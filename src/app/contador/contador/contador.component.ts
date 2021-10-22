650e61123080ec658c9f141811c455f7e710b072
AlexFabra <alfaseart@gmail.com>
2021-10-22 12:50:22 +0200

    error contador solucionado

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contador',
-  templateUrl: './contador.component.html'
+    selector: 'app-contador',
+    templateUrl: './contador.component.html'
 })
 export class ContadorComponent {
 
+    titulo: string = 'Contador App';
+    numero: number = 10;
+    base  : number = 5;
+
+    acumular( valor: number ) {
+        this.numero += valor;
+    }
+    
 }
