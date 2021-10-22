166dfd6ebcccff8ab37f637b2e925b7d58fa0e03
AlexFabra <alfaseart@gmail.com>
2021-10-22 12:02:15 +0200

    primer commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,24 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent {
+    nombre: string = 'Ironman';
+    edad: number = 45;
+    get nombreCapitalizado(): string{
+        return this.nombre.toUpperCase();
+    }
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad}`;
+        // es lo mismo que: return 'this.nombre + " - " + this.edad';
+    }
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+    cambiarEdad(): void {
+        this.edad = 30;
+    }
+}
